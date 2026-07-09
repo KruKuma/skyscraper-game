0de09a1bbef07a4d01d652af299922eefe92d47b
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-09 03:25:49 +0200

    feat(bonus/solve): add dynamic check

--- bonus/solve.c
+++ bonus/solve.c
@@ -11,6 +11,52 @@
 /* ************************************************************************** */
 
 int	check_all_views(int grid[9][9], int views[36], int size);
+int	check_row(int grid[9][9], int row, int left, int right, int size);
+int	check_col(int grid[9][9], int col, int top, int bottom, int size);
+
+int	is_row_complete(int grid[9][9], int size, int row)
+{
+	int	col;
+
+	col = 0;
+	while (col < size)
+	{
+		if (grid[row][col] == 0)
+			return (0);
+		col++;
+	}
+	return (1);
+}
+
+int	is_col_complete(int grid[9][9], int size, int col)
+{
+	int	row;
+
+	row = 0;
+	while (row < size)
+	{
+		if (grid[row][col] == 0)
+			return (0);
+		row++;
+	}
+	return (1);
+}
+
+int	is_still_valid(int grid[9][9], int views[36], int size, int row, int col)
+{
+	if (is_row_complete(grid, size, row))
+	{
+		if (!check_row(grid, row, views[row + size * 2],
+				views[row + size * 3], size))
+			return (0);
+	}
+	if (is_col_complete(grid, size, col))
+	{
+		if (!check_col(grid, col, views[col], views[col + size], size))
+			return (0);
+	}
+	return (1);
+}
 
 int	is_valid_number(int grid[9][9], int size, int row, int col, int nb)
 {
@@ -44,8 +90,11 @@ int	solve(int grid[9][9], int views[36], int pos, int size)
 		if (is_valid_number(grid, size, row, col, nb))
 		{
 			grid[row][col] = nb;
-			if (solve(grid, views, pos + 1, size))
-				return (1);
+			if (is_still_valid(grid, views, size, row, col))
+			{
+				if (solve(grid, views, pos + 1, size))
+					return (1);
+			}
 			grid[row][col] = 0;
 		}
 		nb++;
